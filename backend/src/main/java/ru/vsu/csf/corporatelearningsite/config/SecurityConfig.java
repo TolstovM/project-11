9c48afba57e33be9d715d908610a8d815a16d236
TolstovM <maksfox0@gmail.com>
2020-06-08 14:35:48 +0300

    Доступ к получению своей отметки за курс открыт высем авторизованным пользователям.

--- backend/src/main/java/ru/vsu/csf/corporatelearningsite/config/SecurityConfig.java
+++ backend/src/main/java/ru/vsu/csf/corporatelearningsite/config/SecurityConfig.java
@@ -90,6 +90,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/user/update/authorities", "/api/user/invite", "/api/user/search/findInstructorsByCourseId")
                 .hasRole("ADMIN")
 
+                .antMatchers("/api/course/*/mark")
+                .authenticated()
                 .antMatchers("/api/course/create", "/api/course/*/mark/**")
                 .hasRole("INSTRUCTOR")
                 .antMatchers("/api/course/addListener")
